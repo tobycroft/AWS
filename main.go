6b10d160584f865970cd93ccb57bb41e8bfe9e90
TobyCroft <silverhawk@foxmail.com>
2020-08-16 01:07:18 +0800

    Tuuz

--- main.go
+++ main.go
@@ -19,30 +19,41 @@ func main() {
 			fmt.Printf("err = %s\n", err)
 			return
 		} else {
-			defer func() {
-				// 发送 websocket 结束包
-				conn.WriteMessage(websocket.CloseMessage,
-					websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-				// 真正关闭 conn
-				conn.Close()
-			}()
+			defer on_close(conn)
 			// 读取一个包
-			mt, d, err := conn.ReadMessage()
-
-			if err != nil {
-				fmt.Printf("read fail = %v\n", err)
-				return
-			}
-
-			fmt.Printf("data:%s\n", d)
+			//mt, d, err := conn.ReadMessage()
+			//
+			//if err != nil {
+			//	fmt.Printf("read fail = %v\n", err)
+			//	return
+			//}
+			//
+			//fmt.Printf("data:%s\n", d)
 			// 写入一个包
-			err = conn.WriteMessage(mt, d)
-			if err != nil {
-				fmt.Printf("write fail = %v\n", err)
-				return
-			}
+			//err = conn.WriteMessage(mt, d)
+			//if err != nil {
+			//	fmt.Printf("write fail = %v\n", err)
+			//	return
+			//}
+			on_connect(conn)
 		}
 	})
 
 	r.Run(":80")
+}
+
+func on_connect(conn *websocket.Conn) {
+	err := conn.WriteMessage(200, []byte("连入成功"))
+	if err != nil {
+		fmt.Printf("write fail = %v\n", err)
+		return
+	}
+}
+
+func on_close(conn *websocket.Conn) {
+	// 发送 websocket 结束包
+	conn.WriteMessage(websocket.CloseMessage,
+		websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+	// 真正关闭 conn
+	conn.Close()
 }
