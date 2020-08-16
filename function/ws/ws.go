675a8e4e05f82a1f3150142c7025ecad4edcbd6a
TobyCroft <silverhawk@foxmail.com>
2020-08-16 13:30:48 +0800

    Tuuz

--- function/ws/ws.go
+++ function/ws/ws.go
@@ -11,6 +11,7 @@ import (
 
 var User2Ws map[string]*websocket.Conn
 var Ws2User map[*websocket.Conn]string
+var Room map[*websocket.Conn]int
 
 func On_connect(conn *websocket.Conn) {
 	//err := conn.WriteMessage(1, []byte("连入成功"))
@@ -78,20 +79,65 @@ func Handler(json_str string, conn *websocket.Conn) {
 		} else {
 			rtt, err := Jsong.JObject(ret)
 			if err != nil {
-
+				res := map[string]interface{}{
+					"code": 400,
+					"data": "网络错误请重试",
+					"type": data["type"],
+				}
+				conn.WriteJSON(res)
 			} else {
 				if rtt["code"] == 0 {
 					User2Ws[uid] = conn
 					Ws2User[conn] = uid
+					Room[conn] = 0
+					message := "欢迎" + uid + "连入聊天服务器"
+					if config.DEBUG {
+						fmt.Println(message)
+					}
+					res := map[string]interface{}{
+						"code": 0,
+						"data": "初始化完成",
+						"type": data["type"],
+					}
+					conn.WriteJSON(res)
 				} else {
 					res := map[string]interface{}{
-						"code": 400,
-						"data": "网络错误请重试",
+						"code": -1,
+						"data": "未登录",
 						"type": data["type"],
 					}
 					conn.WriteJSON(res)
 				}
 			}
+		}
+		break
+
+	case "join_room", "JOIN_ROOM":
+		if Ws2User[conn] != "" {
+			if data["chat_type"] == "private" {
+				res := map[string]interface{}{
+					"code": 0,
+					"data": "已经加入和" + Calc.Any2String(data["id"]),
+					"type": data["type"],
+				}
+				conn.WriteJSON(res)
+			} else if data["chat_type"] == "group" {
+				res := map[string]interface{}{
+					"code": 0,
+					"data": "已经加入和" + Calc.Any2String(data["id"]),
+					"type": data["type"],
+				}
+				conn.WriteJSON(res)
+			} else {
+				res := map[string]interface{}{
+					"code": 400,
+					"data": "类型不存在",
+					"type": data["type"],
+				}
+				conn.WriteJSON(res)
+			}
+		} else {
+			conn.WriteJSON(map[string]interface{}{"code": -1, "data": "Auth_Fail", "type": data["type"]})
 		}
 		break
 
