2f42e7ec8488bd92ae38e129c279e5bbaf21704e
TobyCroft <silverhawk@foxmail.com>
2020-08-16 14:31:24 +0800

    Tuuz

--- config/url.go
+++ config/url.go
@@ -8,3 +8,6 @@ const Group_msg = "/api/msg/group_msg"
 const Request_count = "/api/req/count"
 
 const ManualAPI = "/api/msg/"
+
+const Clear_private_unread = "/api/msg/clear_private_unread"
+const Clear_group_unread = "/api/msg/clear_group_unread"
