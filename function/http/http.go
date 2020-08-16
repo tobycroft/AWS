35adb35810bbf688ce60026f45f51c4e7522885b
TobyCroft <silverhawk@foxmail.com>
2020-08-16 11:24:53 +0800

    Tuuz

--- function/http/http.go
+++ function/http/http.go
@@ -1,7 +1,27 @@
 package http
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"main.go/config"
+)
 
-func On(c *gin.Context) {
-
+func Handler(c *gin.Context) {
+	key, ok := c.GetPostForm("key")
+	if !ok {
+		c.JSON(200, map[string]interface{}{
+			"code": 400,
+			"data": "key",
+		})
+		c.Abort()
+		return
+	}
+	if key != config.KEY {
+		c.JSON(200, map[string]interface{}{
+			"code": 403,
+			"data": "key_error",
+		})
+		c.Abort()
+		return
+	}
+	to_user:=
 }
